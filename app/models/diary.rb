4120b32427da834031037d0bf4b5e4d31c52d066
Kazuya-Sakamoto <sskazuya1130@gmail.com>
2019-11-09 18:08:09 +0900

    投稿機能実装
    db　sql　連携

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,5 @@
 class Diary < ApplicationRecord
-    validates :title, :content, :image, presence: true
-    belongs_to :user
+    # validates :title, :content, :image, presence: true
+    # belongs_to :user
+    mount_uploader :image, ImagesUploader
 end
