93264c97d36f444d24b85096b88c1a3d1a28a994
Kazuya-Sakamoto <sskazuya1130@gmail.com>
2019-11-10 19:26:03 +0900

    user-profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitzer.permit(:sign_up, key: [:nickname])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :image])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'diaries#index'
   resources :diaries, only: [:new, :create, :show, :destroy, :update, :edit]
+  resources :users, only: :show
 end
