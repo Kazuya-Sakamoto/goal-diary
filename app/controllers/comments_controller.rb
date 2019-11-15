403ff18875d26a51d4a1b5e658f7e74167b91c38
Kazuya-Sakamoto <sskazuya1130@gmail.com>
2019-11-16 01:11:21 +0900

    修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
     def create
       comment = Comment.create(comment_params)
-      redirect_to diary_path(comment.diary.id)
+      redirect_to diary_path(comment.diary_id)
     end
 
     private
