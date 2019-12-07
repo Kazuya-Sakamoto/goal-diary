7c9ae33fa38f029ad8cbbfe620fe633b6b5379eb
Kazuya-Sakamoto <sskazuya1130@gmail.com>
2019-12-07 15:12:45 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,24 +6,23 @@ class LikesController < ApplicationController
     @like = Like.new(like_params)
     if @like.save
       respond_to do |format|
-        # format.html { redirect_to diaries_path, notice: 'いいねしました'}
+        format.html { redirect_to diaries_path, notice: 'いいねしました'}
         format.json 
       end
     end
   end
 
   def destory
-    @diary = Diary.find(params[:diary_id])
-    @like = Like.destory(like_params)
-    if respond_to do |format|
-      format.json
-    end
+    # @diary = Diary.find(params[:diary_id])
+    # @like = Like.destory(like_params)
+    # if respond_to do |format|
+    #   format.json
+    # end
   end
 
-    private
+  private
     def like_params
       params.permit().merge(user_id: current_user.id, diary_id: params[:diary_id])
     end
-  end
 end
 
