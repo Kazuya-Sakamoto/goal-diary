83492eea633970b87a3faf49a261ce40a6860081
Kazuya-Sakamoto <sskazuya1130@gmail.com>
2019-12-07 14:58:42 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,29 +1,28 @@
-# class LikesController < ApplicationController
-#     before_action :current_user
+class LikesController < ApplicationController
+  before_action :current_user
 
-#     def create
-#       @diary = Diary.find(params[:diary_id])
-#       @like = Like.new(like_params)
-#       if @like.save
-#         respond_to do |format|
-#           # format.html { redirect_to diaries_path, notice: 'いいねしました'}
-#           format.json 
-#         end
-#       end
-#     end
+  def create
+    @diary = Diary.find(params[:diary_id])
+    @like = Like.new(like_params)
+    if @like.save
+      respond_to do |format|
+        # format.html { redirect_to diaries_path, notice: 'いいねしました'}
+        format.json 
+      end
+    end
+  end
 
-#     def destory
-#       @diary = Diary.find(params[:diary_id])
-#       @like = Like.destory(like_params)
-#       if respond_to do |format|
-#         format.json
-#       end
-#     end
+  def destory
+    @diary = Diary.find(params[:diary_id])
+    @like = Like.destory(like_params)
+    if respond_to do |format|
+      format.json
+    end
+  end
 
-#     private
-#     def like_params
-#       params.permit().merge(user_id: current_user.id, diary_id: params[:diary_id])
-#     end
-#   end
-# end
+  private
+  def like_params
+    params.permit().merge(user_id: current_user.id, diary_id: params[:diary_id])
+  end
+end
 
