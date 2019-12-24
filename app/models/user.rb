646eab55a75aabb146aa718b1b1d8a00eb6ef5f5
Kazuya-Sakamoto <sskazuya1130@gmail.com>
2019-12-24 18:42:16 +0900

    likes.create

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
   has_many :diaries
   has_many :goals
   has_many :comments
-  has_many :likes
+  has_many :likes, dependent: :destroy
+  has_many :liked_diaries, through: :likes, source: :post
   # validates :introduction, length: { in: 1.1000}
+  def already_liked?(diary)
+    self.likes.exists?(diary_id: diary.id)
+  end
 end
