3e4df184234e913c736f7604c4bba13ed74b37b5
Kazuya-Sakamoto <sskazuya1130@gmail.com>
2019-12-25 12:41:50 +0900

    refactoring.controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    # User.edit(user_params)
     @diaries = @user.diaries.page(params[:page]).per(12).order("created_at DESC") 
   end
 
   def edit
     @user = User.find(params[:id])
-    # @users = User.edit(user_params)
   end
 
   def update
