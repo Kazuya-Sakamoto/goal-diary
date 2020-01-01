2607786299dca9215ec7b700d5befcb8ead8f90a
Kazuya-Sakamoto <sskazuya1130@gmail.com>
2020-01-02 02:31:06 +0900

    error

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  root to: 'diaries#top'
+  root to: 'diaries#index'
+  # root to: 'diaries#top'
   resources :diaries do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: :create
